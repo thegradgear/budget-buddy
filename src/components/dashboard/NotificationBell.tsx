d8cd804b4e8705a109d125f4755161bc8acb582c
gradgear <gradgear7@gmail.com>
2025-07-08 17:19:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/dashboard/NotificationBell.tsx
+++ src/components/dashboard/NotificationBell.tsx
@@ -133,7 +133,7 @@ export default function NotificationBell() {
     return (
         <Popover open={isOpen} onOpenChange={handleOpenChange}>
             <PopoverTrigger asChild>
-                <Button variant="ghost" size="icon" className="relative">
+                <Button variant="ghost" size="icon" className="relative data-[state=open]:bg-accent">
                     <BellIcon />
                     {unreadCount > 0 && (
                         <Badge variant="destructive" className="absolute -top-1 -right-1 h-5 w-5 p-0 flex items-center justify-center text-xs">
