9b181edd8c71ec32e8139d56624abd0db7d4b5ce
gradgear <gradgear7@gmail.com>
2025-07-14 16:30:36 +0000

    if there is unused code or unnecessary code in the codebase, remove such

--- src/app/dashboard/budget/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-// This page is no longer in use and will be removed in a future update.
-// The budget functionality has been moved to a component on the main dashboard.
-export default function BudgetPage() {
-  return null;
-}
